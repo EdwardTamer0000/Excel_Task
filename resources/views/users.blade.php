abcdcadb646993b472e2fd723a71b9fabfd7ade6
EdwardTamer0000 <142928418+EdwardTamer0000@users.noreply.github.com>
2023-09-02 18:30:11 +0300

    first commit

---
+++ resources/views/users.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>import Laravel from mcv or excel sheet</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></head>
+<body>
+     
+<div class="container">
+    <div class="card mt-3 mb-3">
+        <div class="card-header text-center">
+            <h4> internship task</h4>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('users.import') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <input type="file" name="file" class="form-control">
+                <br>
+                <button class="btn btn-primary">Import User Data</button>
+            </form>
+  
+            <table class="table table-bordered mt-3">
+                <tr>
+                    <th colspan="3">
+                        List Of Users
+                        <a class="btn btn-danger float-end" href="{{ route('users.export') }}">Export User Data</a>
+                    </th>
+                </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                </tr>
+                @endforeach
+            </table>
+  
+        </div>
+    </div>
+</div>
+     
+</body>
+</html>
